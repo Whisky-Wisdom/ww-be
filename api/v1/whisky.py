30c25f24d5faee4e6dc16afc7faa3dfe574351c7
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-05-15 16:32:49 +0900

    일단 글자 추출 가능

--- api/v1/whisky.py
+++ api/v1/whisky.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from services.whisky_service import get_all_whiskies
+from services.data_collection.youtube_url_to_text import process_youtube_url_to_text
 
 router = APIRouter()
 
@@ -7,5 +8,26 @@ router = APIRouter()
 def list_whiskies():
     return get_all_whiskies()
 
+
+
+
+@router.post("/youtube_url_to_text")
+async def youtube_url_to_text(request: Request):
+    body = await request.json()
+    url = body.get("url")
+    if not url:
+        return ({
+            'text': 'text가 없습니다',
+        })
+    # todo 윗부분 공통화 작업 필요
+
+    if url.startswith('https://www.youtube.com') or url.startswith('https://m.youtube.com'):
+        text = process_youtube_url_to_text(url)
+    else:
+        return ({
+            'text': '유튜브 영상이 아닙니다',
+        })
+
+    return text
 
 
---
+++ services/data_collection/youtube_url_to_text.py
@@ -0,0 +1,68 @@
+import uuid
+import yt_dlp
+from pydub import AudioSegment
+import os
+import whisper
+import torch
+
+
+def process_youtube_url_to_text(url):
+
+
+    # 동영상 다운로드
+    mp4_file = download_video(url)
+    print("동영상 다운 완료")
+    # 오디오 추출
+    audio_file = extract_audio_from_video(mp4_file)
+    print("오디오 추출 완료")
+    
+    # 오디오에서 텍스트 추출
+    text = transcribe_audio_to_text(audio_file)
+
+    print("텍스트 추출 완료")
+
+    # 파일 삭제
+    cleanup_files(mp4_file, audio_file)
+    
+    print("파일 삭제 완료")
+
+    return {"text": text}
+
+
+
+
+# 1. datatemp 디렉토리에 유튜브 URL에서 동영상을 다운로드하고 MP4로 저장
+def download_video(url):
+    filename = f"{uuid.uuid4()}.mp4"
+    ydl_opts = {
+        'format': 'mp4',
+        'outtmpl': filename,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    return filename
+
+# 2. MP4 파일에서 오디오를 추출
+def extract_audio_from_video(mp4_file):
+    audio_file = mp4_file.replace('.mp4', '.wav')
+    video = AudioSegment.from_file(mp4_file, format="mp4")
+    video.export(audio_file, format="wav")
+    return audio_file
+
+
+
+# 3. 오디오에서 텍스트를 추출 (음성 인식)
+def transcribe_audio_to_text(audio_file):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = whisper.load_model("medium", device=device)  # GPU 사용 (cuda)
+
+    result = model.transcribe(audio_file, language='ko')  # GPU에서 FP16 자동 적용
+    return result["text"]
+
+
+# 4. 동영상 및 오디오 파일 삭제
+def cleanup_files(mp4_file, audio_file):
+    if os.path.exists(mp4_file):
+        os.remove(mp4_file)
+    if os.path.exists(audio_file):
+        os.remove(audio_file)
